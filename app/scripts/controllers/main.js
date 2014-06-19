b8acd3559efef36a3655db194a82263c21f30856
Alex Voerman <avoerman89@gmail.com>
2014-06-18 22:32:14 -0500

    Putting nulls last might make more sense too.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -199,4 +199,16 @@ angular.module('ncaabbRankSiteApp')
                 reverseSortByDefault: true
             }];
         }
+    })
+    .filter('nullsLast', function() {
+        return function(array, key) {
+            if (!angular.isArray(array)) return;
+            var present = array.filter(function(item) {
+                return item[key];
+            });
+            var empty = array.filter(function(item) {
+                return !item[key]
+            });
+            return present.concat(empty);
+        };
     });
