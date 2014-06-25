6d2c251001bde5a561b5fb0ef6842b5f6ea3df46
Alex Voerman <avoerman89@gmail.com>
2014-06-24 22:39:30 -0500

    Finding a way to mark quality wins

--- app/scripts/controllers/teamController.js
+++ app/scripts/controllers/teamController.js
@@ -6,7 +6,7 @@ angular.module('ncaabbRankSiteApp')
         /* For now, we're just going to get the rankings data over again */
         $http.get('data/rankings/alldata.json').success(function(data) {
             var teams = data.filter(function(fteam) {
-                return fteam.name === $routeParams.name;;
+                return fteam.name === $routeParams.name;
             });
 
             $scope.team = teams[0];
@@ -14,7 +14,7 @@ angular.module('ncaabbRankSiteApp')
 
         $http.get('data/schedules/scheduledata.json').success(function(data) {
             var scheduleTeams = data.filter(function(fteam) {
-                return fteam.name === $routeParams.name;;
+                return fteam.name === $routeParams.name;
             });
 
             $scope.contests = scheduleTeams[0].schedule;
@@ -23,7 +23,7 @@ angular.module('ncaabbRankSiteApp')
         $scope.getBMPercentBrackets = function(totalBrackets) {
             var percentage = 0;
             if (totalBrackets > 0) {
-                percentage = parseFloat((totalBrackets / 121) * 100).toFixed(2).replace(/\.?0+$/, "");
+                percentage = parseFloat((totalBrackets / 121) * 100).toFixed(2).replace(/\.?0+$/, '');
             }
             return percentage + '%';
         };
@@ -38,5 +38,37 @@ angular.module('ncaabbRankSiteApp')
                     return '';
             }
 
+        };
+
+        $scope.getQuality = function(contest) {
+            if (contest.outcome === 'W') {
+                if (contest.rpi <= 50) {
+                    if (contest.location === 'A') {
+                        return 'TOP50 ROAD';
+                    } else {
+                        return 'TOP50';
+                    }
+                } else if (contest.rpi <= 100 && contest.rpi > 50) {
+                    if (contest.location === 'A') {
+                        return 'TOP100 ROAD';
+                    } else {
+                        return 'TOP100';
+                    }
+                }
+            } else {
+                if (contest.rpi >= 300) {
+                    if (contest.location === 'H') {
+                        return '300+ HOME';
+                    } else {
+                        return '300+';
+                    }
+                } else if (contest.rpi >= 200 && contest.rpi < 300) {
+                    if (contest.location === 'H') {
+                        return '200+ HOME';
+                    } else {
+                        return '200+';
+                    }
+                }
+            }
         };
     });
